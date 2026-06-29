38d6107a961b6b9cace42c2975a565cbd7295ce2
RudDavid <satorugodze17@gmail.com>
2026-06-29 08:47:02 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace AbstractFactory {
+  interface IButton {
+    void Draw();
+  }
+
+  interface ITextBox {
+    void Show();
+  }
+
+  class WindowsButton : IButton {
+    public void Draw() {
+      Console.WriteLine("Кнопка Windows");
+    }
+  }
+
+  class WindowsTextBox : ITextBox {
+    public void Show() {
+      Console.WriteLine("Поле Windows");
+    }
+  }
+
+  // Интерфейс фабрики
+  interface IGUIFactory {
+    IButton CreateButton();
+    ITextBox CreateTextBox();
+  }
+
+  // Фабрика Windows
+  class WindowsFactory : IGUIFactory {
+    public IButton CreateButton() {
+      return new WindowsButton();
+    }
+
+    public ITextBox CreateTextBox() {
+      return new WindowsTextBox();
+    }
+  }
+
+  class Program {
+    static void Main(string[] args) {
+      IGUIFactory factory = new WindowsFactory();
+
+      IButton button = factory.CreateButton();
+      ITextBox textBox = factory.CreateTextBox();
+
+      button.Draw();
+      textBox.Show();
+    }
+  }
+}
