7a2d32b038a260178ccd69c2ee58085ef950d560
wacpercs <wacpercs@gmail.com>
2026-06-29 09:04:58 +0700

    added comments for best responsibility

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+# интерфейс визитора
 interface IVisitor {
     void Visit(Book book);
 }
 
+# интерфейс должен реализовывать все элементы поэтому есть Accept
 interface IElement {
     void Accept(IVisitor visitor);
 }
 
+# гет сет хранит название книги, а конструктор получает название и сейвит. Класс книги реализует интерфейс из за этого метод Accept реализуется
 class Book : IElement {
     public string Name { get; set; }
 
@@ -18,12 +21,14 @@ class Book : IElement {
     }
 }
 
+# класс где посетитель реализует интерфейс
 class PrintVisitor : IVisitor {
     public void Visit(Book book) {
         Console.WriteLine($"Книга: {book.Name}");
     }
 }
 
+#мэйн где создается книга и посетитель, там книга принимает визитора, который вызывает метод Визит и выводится книга
 class Program {
     static void Main() {
         Book book = new Book("Book#1");
