3feb1aa8f2d8dad16808b3137e3ec9c4b72a1d49
Kirill <ksmushkov@bk.ru>
2026-06-29 09:26:39 +0700

    реализовал MVC паттерн

---
+++ MVC/MVC/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// Модель
+class Model
+{
+    public string Text = "Hello";
+}
+
+// Представление
+class View
+{
+    public void Show(string text)
+    {
+        Console.WriteLine(text);
+    }
+}
+
+// Контроллер
+class Controller
+{
+    private Model model = new Model();
+    private View view = new View();
+
+    // Получаем данные из модели
+    // и передаем их в представление
+    public void Display()
+    {
+        view.Show(model.Text);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создаем контроллер
+        Controller controller = new Controller();
+
+        // Отображаем данные
+        controller.Display();
+    }
+}
